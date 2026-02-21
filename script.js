adffacfabd6e7655ff7476773902cd6da31a0e97
Trang <dothuytrang2312@gmail.com>
2026-02-21 14:46:49 +0100

    Create users drop down selector

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 import { getUserIds } from "./storage.js";
 
-window.onload = function () {
+const selectedUser = document.getElementById("users-selector")
+
+// make selector to choose user to display bookmarks
+function makeDisplayUserSelector() {
   const users = getUserIds();
-  document.querySelector("body").innerText = `There are ${users.length} users`;
+  for (let i = 1; i < users.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `User ${i}`;
+    option.value = i
+    selectedUser.append(option)
+  }
 };
+
+makeDisplayUserSelector()
