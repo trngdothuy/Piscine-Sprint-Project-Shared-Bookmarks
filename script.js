04f54f88423bbdf06af669acdc4dd271afd17a24
Trang <dothuytrang2312@gmail.com>
2026-02-21 17:30:33 +0100

    Display bookmarks of multi-users

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 import { getUserIds, getData } from "./storage.js";
 
 const selectedUser = document.getElementById("users-selector")
+const bookmarkDisplayDiv = document.getElementById("bookmark-display-div")
 
 let state = {
   userId: "1",
+  bookmarksData: {},
 }
 
 // make selector to choose user to display bookmarks
@@ -18,12 +20,38 @@ function makeDisplayUserSelector() {
 };
 
 makeDisplayUserSelector()
+displayBookmarks(state.userId)
 
 selectedUser.addEventListener("change", (e) => {
-  e.preventDefault
-  state.userId = e.target.value
+  state.userId = e.target.value;
+  bookmarkDisplayDiv.innerHTML = ""
+  displayBookmarks(state.userId)
+  console.log(state)
 })
 
-console.log(getData("1"))
+function displayBookmarks(userId) {
+  state.bookmarksData = getData(userId)
+  const bookmarkShown = state.bookmarksData.map(makeBookmarkCard)
+  bookmarkDisplayDiv.append(...bookmarkShown)
+}
+
+
+function makeBookmarkCard({name, url, description, timestamp}) {
+  const bookmarkCardDiv = document.createElement("div");
+  bookmarkCardDiv.className = "bookmark-card-div";
+
+  const bookmarkName = document.createElement("a")
+  bookmarkName.href = url
+  bookmarkName.innerHTML = name
+
+  const bookmarkDescription = document.createElement("p")
+  bookmarkDescription.innerHTML = description
+
+  const bookmarkTimestamp = document.createElement("p")
+  bookmarkTimestamp.innerHTML = new Date(timestamp).toLocaleDateString() 
+
+  bookmarkCardDiv.append(bookmarkName, bookmarkDescription, bookmarkTimestamp)
+  return bookmarkCardDiv;
+}
 
 
